19bcc07ea48629bdf52b7cc2476bcce441cf4309
Nameless <badxnameless@gmail.com>
2019-09-05 22:29:38 +0100

    Fix remove emoji

--- src/components/TitlePreview/TitlePreview.tsx
+++ src/components/TitlePreview/TitlePreview.tsx
@@ -146,7 +146,12 @@ const TitlePreview = () => {
       return;
     }
     setSteamImgSrc(details.avatarUrl);
-    setSteamName(details.username);
+    setSteamName(
+      details.username.replace(
+        /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g,
+        ""
+      )
+    );
   };
 
   const handleGlowDisableToggle = (glowEnabledInput: boolean) => {
--- src/components/TitlePreviewForm/TitlePreviewForm.tsx
+++ src/components/TitlePreviewForm/TitlePreviewForm.tsx
@@ -87,14 +87,7 @@ const TitlePreviewForm = (props: ITitlePreviewFormProps) => {
         />
         <Button
           onClick={() => {
-            onSteamIdUpdated(
-              steamId64
-                .replace(/ /g, "")
-                .replace(
-                  /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g,
-                  ""
-                )
-            );
+            onSteamIdUpdated(steamId64.replace(/ /g, ""));
           }}
           color="primary"
         >
